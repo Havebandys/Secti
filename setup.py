85fcfafd38d5d0d1e39883aa3fb3cf21db0d9db4
Juan Pablo Pisano <flodalila@gmail.com>
2024-12-03 14:32:17 +0100

    add dependencies to pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="secfi",  
-    version="0.1.10",
+    version="0.1.11",
     description=" Python tool to collect SEC filings for all publicly traded companies. Easily fetch forms like 10-K, 10-Q, and 8-K, along with links and document contents. Ideal for analysts, researchers, and anyone exploring financial reports or SEC data. Simplify your access to essential company information",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
